ccfc766327c08fd3a2a448d9a03bd6f348770507
Davi Milioli <davi.milioli@inforce.com.br>
2025-01-22 13:26:35 -0300

    refact: objetos tirado do model

--- src/models/ResponseHandler.ts
+++ src/models/ResponseHandler.ts
@@ -2,9 +2,5 @@ export default interface ResponseHandler {
     status: string;
     message: string;
     statusCode: number;
-    data?: {
-        user: string;
-        token: string;
-        expiration: string
-    }
+    data?: {}
 }
